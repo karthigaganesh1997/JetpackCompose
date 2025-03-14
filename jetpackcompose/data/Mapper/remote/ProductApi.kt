c7ec99b994355fe496f01ea86a9167ab56cca555
karthigaganesh1997 <57249842+karthigaganesh1997@users.noreply.github.com>
2025-03-14 20:00:03 +0530

    Delete jetpackcompose directory

--- jetpackcompose/data/Mapper/remote/ProductApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jetpackcompose.data.remote
-
-import com.example.jetpackcompose.domain.model.Product
-import retrofit2.http.GET
-
-interface ProductApi {
-    @GET("products")
-    suspend fun getProducts()  : List<Product>
-}
--- jetpackcompose/util/Constant.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jetpackcompose.util
-
-object Constant{
-const val  BASE_URL = "https://fakestoreapi.com/"
-}
